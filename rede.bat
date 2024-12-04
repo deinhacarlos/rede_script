a2aafb7e612b2aa8782bc00539cfcca376b8235e
AndreaSantos <andrea.santos107@etec.sp.gov.br>
2024-12-03 22:47:56 -0300

    Script de criação de pastas

---
+++ rede.bat
@@ -0,0 +1,96 @@
+@echo off
+
+cls
+cd\
+echo "Criando pasta de rede"
+pause
+md rede
+cd rede
+echo "Listando conteúdo pasta de rede"
+pause
+dir
+echo "Criando pastas dos colaboradores"
+pause
+
+md ALAN-SOARES
+
+md ANDREA-SANTOS
+
+md BRUNO-OLIVEIRA
+
+md CAMILA-LIRA
+
+md CARLOS-SOUSA
+
+md CAROLINE-NASCIMENTO
+
+md DANIEL-SANTANA
+
+md DARIO-FARIAS
+
+md EDUARDO-RIOS
+
+md FABIO-SILVA
+
+md FELIPE-MACHADO
+
+md FELIPE-BARRIOS
+
+md FLÁVIO-SOUZA
+
+md FRANCISCO-CAVALCANTE
+
+md GABRIEL-CREPALDI
+
+md GABRIEL-LIMA
+
+md GABRIEL-RODRIGUES
+
+md GABRIELY-SILVA
+
+md GUILHERME-ALVES
+
+md GUSTAVO-TAVARES
+
+md HENRIQUE-CABRAL
+
+md ISAAC-SOUZA
+
+md ISABELLY-MESQUITA
+
+md JOÃO-SOARES
+
+md JONATHAN-FREITAS
+
+md KAUÃ-MOTA
+
+md LUCAS-SANTOS
+
+md LUCAS-ANDRADE
+
+md MATHEUS-CARVALHO
+
+md NATALIA-MOREIRA
+
+md NICOLAS-NASCIMENTO
+
+md PALLOMA-SANTOS
+
+md RAISSA-CARMO
+
+md RAPHAELA-MELO
+
+md REBECCA-GUEDES
+
+md RIAM-ANDRE
+
+md ROGERIO-JUNIOR
+
+md SABRINA-BENEVIDES
+
+md WALDANIA-OLIVEIRA
+
+echo "Listando conteúdo pastas criadas"
+
+dir
+pause
